0c1f5f80a1d7beaf30427b8175f8cc2cdb419c9b
alexeei <megaxaser@gmail.com>
2022-10-23 20:33:18 +0300

    fix parts touchsampling, change dimens extra padding

--- parts/src/org/lineageos/devicesettings/touchsampling/TouchSamplingSettingsActivity.java
+++ parts/src/org/lineageos/devicesettings/touchsampling/TouchSamplingSettingsActivity.java
@@ -17,9 +17,11 @@
 package org.lineageos.devicesettings.touchsampling;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class TouchSamplingSettingsActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class TouchSamplingSettingsActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_HTSR = "touchsampling";
 
@@ -27,7 +29,7 @@ public class TouchSamplingSettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new TouchSamplingSettingsFragment(), TAG_HTSR).commit();
     }
 }
