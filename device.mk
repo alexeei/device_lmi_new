9fa87ed8bf97cd9dbd38b8812d90e5bf9051a8b6
alexeei <megaxaser@gmail.com>
2022-10-28 19:44:42 +0300

    Display Config: Use versions up to 1.20

--- device.mk
+++ device.mk
@@ -254,10 +254,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml \
     memtrack.kona \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.11.vendor \
-    vendor.display.config@2.0 \
-    vendor.display.config@2.0.vendor \
+    vendor.display.config@1.20 \
+    vendor.display.config@1.20.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
