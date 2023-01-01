50d3e4350a4ae7652f34cdfae959d9101ddbcc31
alexeei <megaxaser@gmail.com>
2023-01-01 15:36:29 +0300

    Switch to Audio Hal v7.0-7.1

--- device.mk
+++ device.mk
@@ -114,6 +114,21 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-util \
+    libaudiohal@7.0 \
+    libaudiohal@7.1 \
+    android.hardware.audio.common@7.0 \
+    android.hardware.audio.common@7.0-util \
+    android.hardware.audio.common@7.1-enums \
+    android.hardware.audio.common@7.1-util \
+    android.hardware.audio.effect@7.0 \
+    android.hardware.audio.effect@7.0-util \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@7.0 \
+    android.hardware.audio@7.1 \
+    android.hardware.audio@7.1-util \
+    android.hardware.audio@7.1-impl \
+    libaudiohal.effect@7.0 \
     audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
@@ -164,7 +179,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
