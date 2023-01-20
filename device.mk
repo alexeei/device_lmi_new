6acd67e4bbed630ff87cae3b48b145c42d178e3f
markakash <akashniki@gmail.com>
2023-01-20 20:14:36 +1000

    lmi: Add support for misys * System looks for it 10-19 08:53:42.983   616   616 I hwservicemanager: getTransport: Cannot find entry vendor.xiaomi.hardware.misys@1.0::IMiSys/default in either framework or device VINTF manifest. 10-19 08:53:42.983  5015  5054 W System.err: java.util.NoSuchElementException

--- device.mk
+++ device.mk
@@ -328,6 +328,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory.block@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
