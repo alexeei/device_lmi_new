9ed4fba5c303554f9141c17b27d1aa53ab789d63
alexeei <megaxaser@gmail.com>
2023-01-21 17:00:36 +0300

    implement new libperfmanager from pixel

--- device.mk
+++ device.mk
@@ -473,7 +473,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.lmi-libperfmgr
    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json 
