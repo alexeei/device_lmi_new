06d3586c6acaa9abe689d9e94b9926453a18d043
alexeei <megaxaser@gmail.com>
2023-07-02 19:34:56 +0300

    add bluetooth_audio _2.0

--- device.mk
+++ device.mk
@@ -218,6 +218,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     vendor.qti.hardware.fm@1.0.vendor
