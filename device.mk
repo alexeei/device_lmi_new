a2b53a543598943037eb44e613152106c7968214
alexeei <megaxaser@gmail.com>
2023-08-15 22:37:04 +0300

    change name of linperfmanager

--- device.mk
+++ device.mk
@@ -539,7 +539,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-lmi
    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json 
