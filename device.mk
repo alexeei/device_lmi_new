b2cf64b5c7947ba4f9c576e47d37271765117b61
alexeei <megaxaser@gmail.com>
2024-07-17 23:17:56 +0300

    fixes for qpr3

--- device.mk
+++ device.mk
@@ -549,6 +549,7 @@ PRODUCT_PACKAGES += \
    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json 
+
      
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
