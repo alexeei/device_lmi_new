3d927c8f78113557ae6318365ba8bf6882b6b46e
alexeei <megaxaser@gmail.com>
2024-11-14 13:38:58 +0300

    Use common libqti-perfd-client and power-libperfmgr

--- device.mk
+++ device.mk
@@ -559,7 +559,8 @@ PRODUCT_PACKAGES += \
     
 
 # Power
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
+     android.hardware.power-service.lineage-libperfmgr 
 #    android.hardware.power-service.xiaomi-lmi
    
 PRODUCT_COPY_FILES += \
@@ -694,6 +695,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
